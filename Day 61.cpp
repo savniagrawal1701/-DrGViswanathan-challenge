f52ee2a42a63da12e9c06db259819012fcee3261
Savni agrawal <savniagrawal794@gmail.com>
2026-03-18 12:13:18 +0530

    Create Day 61.cpp

---
+++ Day 61.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(), people.end());
+        int n = people.size();
+        int i = 0;
+        int j = n - 1;
+        int count = 0;
+        while (i <= j) {
+
+            if (people[i] + people[j] <= limit) {
+                i++;
+            }
+            j--;
+            count++;
+        }
+        return count;
+    }
+};
